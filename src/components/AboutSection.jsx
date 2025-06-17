90ea0788c2169fbc8958297f55b44c449080aa43
Uday <uday@7ae13539-6bb6-4e25-a63a-5f13ff905fd8.attlocal.net>
2025-06-16 22:20:19 -0500

    images aligned

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -12,7 +12,7 @@ export const AboutSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
-              Passionate Web Developer & Tech Creator
+              Passionate Software Developer 
             </h3>
 
             <p className="text-muted-foreground">
